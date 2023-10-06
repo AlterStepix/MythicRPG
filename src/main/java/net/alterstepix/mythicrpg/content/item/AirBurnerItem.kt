74d3b7ecf64367dc9a6502104b6ab60587fe5030
Crutogurch <crutogurch@yandex.ru>
2023-10-06 20:05:16 +0500

    air burner small update

--- src/main/java/net/alterstepix/mythicrpg/content/item/AirBurnerItem.kt
+++ src/main/java/net/alterstepix/mythicrpg/content/item/AirBurnerItem.kt
@@ -1,6 +1,5 @@
 package net.alterstepix.mythicrpg.content.item
 
-import net.alterstepix.mythicrpg.system.event.MEvent
 import net.alterstepix.mythicrpg.system.event.item.MItemEvent
 import net.alterstepix.mythicrpg.system.item.MythicItem
 import net.alterstepix.mythicrpg.system.manager.MythicContent
@@ -10,19 +9,41 @@ import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.entity.LivingEntity
 import org.bukkit.inventory.ItemStack
-import org.bukkit.potion.PotionEffect
+import kotlin.random.Random
 
 @MythicContent class AirBurnerItem: MythicItem() {
     init {
         registerItemAbility(cooldownMs = 13000L, autoCancel = true) { event : MItemEvent.RightClick, _ ->
             val entities = searchEntities<LivingEntity>(event.player, 10.0)
             if (entities.isEmpty()) {
-                burnEntity(mutableListOf(event.player))
+                burnEntity(mutableListOf(event.player), 1.0, 8)
                 return@registerItemAbility
             }
 
             particles(Particle.SMOKE_LARGE, 200, extra = 0.1).setForce(true).displaySphere(event.player.centerMLoc, 5.0)
-            burnEntity(entities)
+            burnEntity(entities, 1.0, 8)
+
+            if(Random.nextDouble() <= 0.10){
+                burnEntity(mutableListOf(event.player), 0.25, 4)
+            }
+
+        }
+
+        registerItemEvent {event: MItemEvent.AttackLivingEntity ->
+            if(event.player.attackCooldown < 1.0) {
+                particles(Particle.SMOKE_LARGE, 8, extra = 0.4).display(event.player.eyeMLoc)
+                return@registerItemEvent
+            }
+
+            particles(Particle.ITEM_CRACK, 15, data = ItemStack(Material.FLINT_AND_STEEL), extra = 0.3).display(event.target.eyeMLoc)
+            event.target.makeSound(Sound.ENTITY_ITEM_BREAK,0.5f, 0.0f)
+
+            if(Random.nextDouble() <= 0.70){
+                burnEntity(mutableListOf(event.target), 0.5, 4)
+            }else {
+                burnEntity(mutableListOf(event.player), 0.5, 4)
+            }
+
         }
     }
 
@@ -31,30 +52,35 @@ import org.bukkit.potion.PotionEffect
         return ItemStack(Material.FLINT_AND_STEEL)
             .withDisplayName("#F56E3B", "Air Burner")
             .withDamage(0.5)
-            .withAttackSpeed(3.0)
+            .withAttackSpeed(1.0)
             .withAbilityDescription(
                 abilityName = "Hot Air",  activation = "Right Click",
                 "Attacks entities within a radius of *10* blocks,",
                 "dealing fire damage to them over *6.5* seconds,",
-                "but if they are *absent*, it attacks you!"
+                "but if they are *absent* or you'll be out of luck, it attacks you!"
+            )
+            .withAbilityDescription(
+                abilityName = "Unintentional discharge",  activation = "Attack",
+                "Has a *high* chance of setting your target on fire, ",
+                "but there is a risk of setting *you* on fire as well!",
             )
     }
 
-    private fun burnEntity(entities: List<LivingEntity>) {
+    private fun burnEntity(entities: List<LivingEntity>, damage: Double, iterations: Int) {
         for (entity in entities) {
             entity.isVisualFire = true
             particles(Particle.LAVA, 15).display(entity.centerMLoc)
 
-            runLater(128) {
+            runLater(16L*iterations) {
                 entity.isVisualFire = false
             }
 
-            runParallelFor(16L, 8) { breakLoop, _ ->
+            runParallelFor(16L, iterations) { breakLoop, _ ->
                 particles(Particle.SMOKE_LARGE, 8, extra = 0.4).display(entity.eyeMLoc)
                 particles(Particle.ITEM_CRACK, 15, data = ItemStack(Material.COOKED_PORKCHOP), extra = 0.3).display(entity.eyeMLoc)
                 entity.makeSound(Sound.BLOCK_FIRE_AMBIENT, 2.5f, 0.0f)
                 entity.noDamageTicks = 0
-                entity.damage(1.0)
+                entity.damage(damage)
 
                 if(entity.isDead) {
                     breakLoop()
