08680a7a3049000e789afa575ad15f4d4077f312
Stepix <darklightgamesz@gmail.com>
2023-10-06 20:13:41 +0500

    Fix

--- src/main/java/net/alterstepix/mythicrpg/content/item/AirBurnerItem.kt
+++ src/main/java/net/alterstepix/mythicrpg/content/item/AirBurnerItem.kt
@@ -9,7 +9,6 @@ import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.entity.LivingEntity
 import org.bukkit.inventory.ItemStack
-import kotlin.random.Random
 
 @MythicContent class AirBurnerItem: MythicItem() {
     init {
@@ -23,14 +22,13 @@ import kotlin.random.Random
             particles(Particle.SMOKE_LARGE, 200, extra = 0.1).setForce(true).displaySphere(event.player.centerMLoc, 5.0)
             burnEntity(entities, 1.0, 8)
 
-            if(Random.nextDouble() <= 0.10){
+            if(random() <= 0.1) {
                 burnEntity(mutableListOf(event.player), 0.25, 4)
             }
-
         }
 
-        registerItemEvent {event: MItemEvent.AttackLivingEntity ->
-            if(event.player.attackCooldown < 1.0) {
+        registerItemEvent { event: MItemEvent.AttackLivingEntity ->
+            if(event.player.attackCooldown < 0.9) {
                 particles(Particle.SMOKE_LARGE, 8, extra = 0.4).display(event.player.eyeMLoc)
                 return@registerItemEvent
             }
@@ -38,12 +36,11 @@ import kotlin.random.Random
             particles(Particle.ITEM_CRACK, 15, data = ItemStack(Material.FLINT_AND_STEEL), extra = 0.3).display(event.target.eyeMLoc)
             event.target.makeSound(Sound.ENTITY_ITEM_BREAK,0.5f, 0.0f)
 
-            if(Random.nextDouble() <= 0.70){
+            if(random() <= 0.7) {
                 burnEntity(mutableListOf(event.target), 0.5, 4)
-            }else {
+            } else {
                 burnEntity(mutableListOf(event.player), 0.5, 4)
             }
-
         }
     }
 
@@ -71,13 +68,13 @@ import kotlin.random.Random
             entity.isVisualFire = true
             particles(Particle.LAVA, 15).display(entity.centerMLoc)
 
-            runLater(16L*iterations) {
+            runLater(16L * iterations) {
                 entity.isVisualFire = false
             }
 
             runParallelFor(16L, iterations) { breakLoop, _ ->
                 particles(Particle.SMOKE_LARGE, 8, extra = 0.4).display(entity.eyeMLoc)
-                particles(Particle.ITEM_CRACK, 15, data = ItemStack(Material.COOKED_PORKCHOP), extra = 0.3).display(entity.eyeMLoc)
+                particles(Particle.ITEM_CRACK, 15, extra = 0.3, data = ItemStack(Material.COOKED_PORKCHOP)).display(entity.eyeMLoc)
                 entity.makeSound(Sound.BLOCK_FIRE_AMBIENT, 2.5f, 0.0f)
                 entity.noDamageTicks = 0
                 entity.damage(damage)
